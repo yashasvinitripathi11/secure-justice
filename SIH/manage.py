a0c176c47228cb5442db7e63289398f57f84afff
Akshat-Srivastava2004 <akshatsri2004alld@gmail.com>
2023-09-08 17:04:59 +0530

    Akshat ka ched

--- SIH/manage.py
+++ SIH/manage.py
@@ -2,7 +2,6 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from dotenv import load_dotenv
 
 
 def main():
@@ -20,9 +19,4 @@ def main():
 
 
 if __name__ == '__main__':
-    load_dotenv()
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SIH.settings")
-
-    from django.core.management import execute_from_command_line
-    execute_from_command_line(sys.argv)
     main()
