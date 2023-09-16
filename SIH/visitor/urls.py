fb82106e790d9a735cc9048bd1c3f6ffb8efad42
Akshat-Srivastava2004 <akshatsri2004alld@gmail.com>
2023-09-16 13:21:03 +0530

    akshat new change

--- SIH/visitor/urls.py
+++ SIH/visitor/urls.py
@@ -5,7 +5,7 @@ from.import views
  
 
 urlpatterns=[
-    path("CL",views.CL,name="CL"),
+    path("visitor",views.CL,name="CL"),
     path("CLR",views.CLR,name="CLR"),
     path("MLR",views.MLR,name="MLR"),
     path("WLR",views.WLR,name="WLR"),
